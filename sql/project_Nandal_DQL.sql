d87a19cb098e2e0767b48cec8adb127b00120acf
homin0203 <kwonhomin317@gmail.com>
2022-12-21 12:50:38 +0900

    메인페이지 및 상세페이지 dql 코드 수정 및 추가
    메인페이지 각 목록 데이터 조회 dql 및
    상세 페이지 날짜 선택 시 해당 클래스 및 선택 날짜의 요일,날짜를 통한 스케줄 데이터 조회 sql문 작성

--- sql/project_Nandal_DQL.sql
+++ sql/project_Nandal_DQL.sql
@@ -1,7 +1,26 @@
+--메인페이지 리뷰 추천유형 조회
+select CLASS_IMG, CLASS_NAME, CLASS_ADDRESS, CLASS_PRICE
+    from CLASS 
+    where class_code in (select ca.CLASS_CODE
+                                from review r join CLASS_APPLY ca on r.REVIEW_CODE = ca.CA_CODE
+                                where r.REVIEW_GROUP = 3)
+;
+--메인 페이지 노출 리뷰 목록 -- distinct은 중복 제거 first_value는 해당 데이터의 첫번째 행인데 partition by로 그룹핑하면 해당 그룹에 첫번째
+select  distinct first_value(rp.RP_ROUTE)  over(partition by review_code) a, r.REVIEW_CONT, REVIEW_CODE, ca.class_code
+    from review r join review_photo rp using(review_code)
+                        join CLASS_APPLY ca on review_code = ca.ca_code
+     where review_grade > 4
+;
+
 --메인,목록 페이지의 클래스 목록 표시 정보
 select CLASS_IMG, CLASS_NAME, AREA_NAME, CLASS_PRICE
     from CLASS join AREA using(AREA_CODE)
     where CLASS_NAME = '키워드' or CLASS_INTRO = '키워드';
+    
+    select CLASS_IMG, CLASS_NAME, CLASS_ADDRESS, CLASS_PRICE
+    from CLASS 
+    where CLASS_NAME like '%크리스마스%';
+    
 --카테고리 선택 시 클래스 정보
 select c.CLASS_IMG, c.CLASS_NAME, c.CLASS_ADRESS, c.CLASS_PRICE
     from CLASS c join CATEGORY cg using(category_code)
@@ -109,13 +128,11 @@ select count(*)
 select C.CLASS_NAME "클래스명", CA.CA_DATE "신청 날짜", CS.CS_STIME "시작시간", 
             CS.CS_FTIME "종료시간", CA.CA_TOTAL "신청 인원", AC.AC_TIME "취소접수 시간", 
             CO.CO_NAME "옵션명", (C.CLASS_PRICE+CO.CO_PRICE)*CA.CA_TOTAL "금액"
-    from class_apply ca join CLASS_SCHEDULE cs using(cs_code,class_code)
+    from (select * from class_apply where CA.MEMBER_ID = '아이디' and CA.CA_CANCEL = 'Y' and CA.CO_CODE is not null ) ca 
+                        join CLASS_SCHEDULE cs using(cs_code,class_code)
                         join class c using(class_code)
                         join class_option co using(co_code)  
                         join APPLY_CANCEL ac on ca.ca_code = ac.ac_code
-    where CA.MEMBER_ID = '아이디'
-            and CA.CA_CANCEL = 'Y'
-            and CA.CO_CODE is not null
 ;
 --내 클래스 취소내역 목록 정보 조회 - 옵션 없는 경우
 select C.CLASS_NAME "클래스명", CA.CA_DATE "신청 날짜", CS.CS_STIME "시작시간", 
@@ -153,15 +170,19 @@ where review_code=?
 select *
     from class
     where class_code = 124
-; -- 상세정보
+; -- 상세정보 해당 클래스의 사진 가져오기
 select *
     from CLASS_PHOTO
-    where class_code = 124
+    where class_code = 26851 and CP_TYPE = 0
 ; -- 사진
 select *
     from CLASS_SCHEDULE
     where class_code = 124
-; -- 일정
+; -- 해당 클래스 코드에 해당 날짜 및 요일을 표함하는 일정 조회
+select CLASS_CODE, CS_STIME, CS_FTIME
+    from CLASS_SCHEDULE
+    where CLASS_CODE = 26851 and BITAND(cs_day, 4) = 4 and TO_DATE('2023-3-21', 'YYYY-MM-DD') between cs_sdate and cs_fdate
+;
 select r.*
     from review r join CLASS_APPLY ca on r.review_code = ca.ca_code
     where ca.class_code = 124
