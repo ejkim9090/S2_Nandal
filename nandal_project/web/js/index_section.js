0aaadfaa6ff8e1b5a9263e6fb25e156558cc1b52
homin0203 <kwonhomin317@gmail.com>
2022-12-23 11:40:05 +0900

    메인페이지 목록 이동 시 값 수정
    메인 페이지 목록 좌우 버튼 클릭 시 translate3d의 x축 좌표 값이 맞지 않아 다음 목록이 잘려 보여서 변경 값 수정

--- nandal_project/web/js/index_section.js
+++ nandal_project/web/js/index_section.js
@@ -3,17 +3,17 @@ window.onload = function() {
     //각 목록 좌우 넘기기
     var a = 0;
     document.getElementsByClassName("rmd_right_btn")[0].onclick = function () {
-        a += -1157;
+        a += -1137;
         var t_value = "translate3d(" + a +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[0].style.transform = t_value;
 
         document.getElementsByClassName("rmd_left_btn")[0].style.display = "inline-block";
-        if(a == -1157*2) {
+        if(a == -1137*2) {
             document.getElementsByClassName("rmd_right_btn")[0].style.display = "none";
         }
     }
     document.getElementsByClassName("rmd_left_btn")[0].onclick = function () {
-        a += +1157;
+        a += +1137;
         var t_value = "translate3d(" + a +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[0].style.transform = t_value;
 
@@ -24,17 +24,17 @@ window.onload = function() {
     }
     var b = 0;
     document.getElementsByClassName("rmd_right_btn")[1].onclick = function () {
-        b += -1157;
+        b += -1137;
         var t_value = "translate3d(" + b +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[1].style.transform = t_value;
 
         document.getElementsByClassName("rmd_left_btn")[1].style.display = "inline-block";
-        if(b == -1157*2) {
+        if(b == -1137*2) {
             document.getElementsByClassName("rmd_right_btn")[1].style.display = "none";
         }
     }
     document.getElementsByClassName("rmd_left_btn")[1].onclick = function () {
-        b += +1157;
+        b += +1137;
         var t_value = "translate3d(" + b +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[1].style.transform = t_value;
 
@@ -45,19 +45,19 @@ window.onload = function() {
     }
     var c = 0;
     document.getElementsByClassName("rmd_right_btn")[2].onclick = function () {
-        c += -1157;
+        c += -1137;
         var t_value = "translate3d(" + c +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[2].style.transform = t_value;
 
         document.getElementsByClassName("rmd_left_btn")[2].style.display = "inline-block";
-        if(c == -1157*2) {
+        if(c == -1137*2) {
             document.getElementsByClassName("rmd_right_btn")[2].style.display = "none";
         }
         console.log(document.getElementsByClassName("rmd_right_btn")[3]);
         console.log("ㅎㅇㅎㅇㅎ");
     }
     document.getElementsByClassName("rmd_left_btn")[2].onclick = function () {
-        c += +1157;
+        c += +1137;
         var t_value = "translate3d(" + c +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[2].style.transform = t_value;
 
@@ -70,17 +70,17 @@ window.onload = function() {
     }
     var d = 0;
     document.getElementsByClassName("rmd_right_btn")[3].onclick = function () {
-        d += -1157;
+        d += -1137;
         var t_value = "translate3d(" + d +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[3].style.transform = t_value;
 
         document.getElementsByClassName("rmd_left_btn")[3].style.display = "inline-block";
-        if(d == -1157*2) {
+        if(d == -1137*2) {
             document.getElementsByClassName("rmd_right_btn")[3].style.display = "none";
         }
     }
     document.getElementsByClassName("rmd_left_btn")[3].onclick = function () {
-        d += +1157;
+        d += +1137;
         var t_value = "translate3d(" + d +"px, 0px, 0px)";
         document.getElementsByClassName("rmd_list_swiper")[3].style.transform = t_value;
 
