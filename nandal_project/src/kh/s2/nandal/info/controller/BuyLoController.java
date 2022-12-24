7bda2bccc31d2dc7f040439e3da3a8643ec0739a
homin0203 <kwonhomin317@gmail.com>
2022-12-24 14:36:34 +0900

    필요 없는 코드 정리
    클래스 신청 시 BuyLoController Gson 객체 미사용으로 생성 코드 제거

--- nandal_project/src/kh/s2/nandal/info/controller/BuyLoController.java
+++ nandal_project/src/kh/s2/nandal/info/controller/BuyLoController.java
@@ -38,7 +38,6 @@ public class BuyLoController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		Gson gson = new Gson();
 		int result = 0;
 		
 		int csCode = 0;
